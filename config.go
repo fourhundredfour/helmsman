644d1381056845804997d8a896255a57ca524113
Daniel Schischkin <daniel@schischkin.info>
2021-05-06 21:33:54 +0200

    Run go fmt for better formatation

--- config.go
+++ config.go
@@ -1,16 +1,16 @@
 package main
 
 type RouteConfiguration struct {
-	Path string `yaml:"path"`
+	Path   string `yaml:"path"`
 	Layout string `yaml:"layout"`
 }
 
 type Configuration struct {
 	Layout struct {
 		Directories struct {
-			Static string `yaml:"static"`
+			Static   string `yaml:"static"`
 			Template string `yaml:"template"`
-			Cache string `yaml:"cache"`
+			Cache    string `yaml:"cache"`
 		} `yaml:"directories"`
 	} `yaml:"layout"`
 	Routes []RouteConfiguration `yaml:"routes"`
